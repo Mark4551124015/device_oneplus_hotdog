b40538c2fb432af96e1e75d147d7d1c496425921
Mark4551124015 <Mark4551124015@gmail.com>
2021-01-27 23:52:01 +0800

    Update lineage_hotdog.mk

--- lineage_hotdog.mk
+++ lineage_hotdog.mk
@@ -15,7 +15,7 @@ $(call inherit-product, device/oneplus/hotdog/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineaege_hotdog
+PRODUCT_NAME := lineage_hotdog
 PRODUCT_DEVICE := hotdog
 PRODUCT_BRAND := OnePlus
 PRODUCT_MODEL := OnePlus 7T Pro
